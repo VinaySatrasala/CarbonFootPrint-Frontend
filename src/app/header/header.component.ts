ee8a0ea3fea816ea823297694ecd33a671085174
vinay satrasala <satrasalavinaykumar.01@gmail.com>
2024-09-03 23:57:27 +0530

    made changes in navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
 
+
+
+
+export class HeaderComponent {
+  isLoggedIn = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Assume you have some logic to determine if the user is logged in or not
+    this.isLoggedIn = !!localStorage.getItem('token'); // Example logic
+  }
+
+  logout(): void {
+    // Perform logout operation
+    localStorage.removeItem('token'); // Example logic
+    this.isLoggedIn = false;
+    this.router.navigate(['/signin']);
+  }
+  signOut(){
+    
+  }
 }
