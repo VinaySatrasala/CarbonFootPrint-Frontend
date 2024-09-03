764a858cc37b75c957804ff7c6855e9c74b7aae1
shahad7 <shahadneeliyatt786@gmail.com>
2024-09-04 00:43:50 +0530

    pie chart actual data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RadialBarChartComponent } from '../radial-bar-chart/radial-bar-chart.component';
 import { AppModule } from '../app.module';
+import { FactorsService } from '../factors.service';
+import { flush } from '@angular/core/testing';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  record: any = {};
+  constructor(private fatcorsService: FactorsService) {}
+  loading: boolean = true;
+  errorWhenFetching: boolean = false;
+  errorMessage: string = '';
+  emissionPerCategory: any = {};
 
+  ngOnInit(): void {
+    this.fatcorsService.fetchCurrentMonthData().subscribe({
+      next: (response) => {
+        if (response) {
+          this.record = response;
+          this.loading = false;
+          this.extractEmissionsPerCategory();
+        } else {
+          this.errorWhenFetching = true;
+        }
+      },
+      error: (err) => {
+        this.errorWhenFetching = true;
+        this.errorMessage = err.error ? err.error : '';
+      },
+    });
+  }
+
+  extractEmissionsPerCategory(): void {
+    Object.keys(this.record).forEach((key, index) => {
+      if (this.record[key].emission != undefined)
+        this.emissionPerCategory[key] = this.record[key].emission;
+    });
+    this.emissionPerCategory = Object.assign({}, this.emissionPerCategory);
+    console.log(this.emissionPerCategory);
+  }
 }
--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import {
   ApexNonAxisChartSeries,
   ApexChart,
   ApexFill,
   ApexDataLabels,
-  ApexLegend
+  ApexLegend,
 } from 'ng-apexcharts';
 
 export type ChartOptions = {
@@ -20,29 +20,53 @@ export type ChartOptions = {
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
-  styleUrl: './pie-chart.component.css'
+  styleUrl: './pie-chart.component.css',
 })
-export class PieChartComponent {
-  public chartOptions: ChartOptions;
+export class PieChartComponent implements OnChanges {
+  public chartOptions: any;
+
+  @Input()
+  emissionPerCategory!: any;
 
   constructor() {
     this.chartOptions = {
-      series: [50, 120, 324, 108, 120, 220], // Example data for the 6 emission details
+      // Example data for the 6 emission details
+      series: [0, 0, 0, 0, 0, 0],
       chart: {
         type: 'pie',
         height: 300,
-        width:250
+        width: 250,
       },
-      labels: ['Electricity', 'Water', 'Dietary', 'Waste', 'LPG', 'Travel'],
+      labels: [
+        'Electricity',
+        'Water',
+        'Dietary',
+        'Waste',
+        'Domestic Fuels',
+        'Travel',
+      ],
       fill: {
-        type: 'solid'
+        type: 'solid',
       },
       dataLabels: {
         enabled: true,
       },
       legend: {
-        position: 'bottom'
-      }
+        position: 'bottom',
+      },
     };
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.chartOptions.series = !this.emissionPerCategory
+      ? [0, 0, 0, 0, 0, 0]
+      : [
+          this.emissionPerCategory['electricity'] || 0,
+          this.emissionPerCategory['water'] || 0,
+          this.emissionPerCategory['dietary_habits'] || 0,
+          this.emissionPerCategory['waste'] || 0,
+          this.emissionPerCategory['fuel_sources'] || 0,
+          this.emissionPerCategory['public_transport'] +
+            this.emissionPerCategory['private_transport'] || 0,
+        ];
   }
 }
